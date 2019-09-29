7c1c8fc2f939e6e72618c5f27f7ed037ea94451b
Jinook <jinook929@gmail.com>
2019-09-28 22:04:08 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+
+//        System.out.println(SpeedConverter.toMilesPerHour(75.114));
+
+        SpeedConverter.printConversion(75.114);
+    }
+}
